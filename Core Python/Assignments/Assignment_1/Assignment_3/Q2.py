8daae5e0b0f10fdac03a992f887367f96f3b0c03
Yash-Patil25 <yp35385@gmail.com>
2025-10-26 17:52:58 +0530

    Delete Core Python/Assignments/Assignment_1/Assignment_3 directory

--- Core Python/Assignments/Assignment_1/Assignment_3/Q2.py
+++
@@ -1,7 +0,0 @@
-#Write a program to input any alphabet and check whether it is vowel or consonant.
-ch = input("Enter Alphabet:")
-
-if ch.lower() in ['a','e','i','o','u']:
-    print(f'{ch} is vowel.')
-else:
-    print(f'{ch} is a consonant.')
--- Core Python/Assignments/Assignment_1/Assignment_3/Q3.py
+++
@@ -1,9 +0,0 @@
-#Write a program to input angles of a triangle and check whether triangle is valid or not.
-angle1 = float(input("Enter angle:"))
-angle2 = float(input("Enter angle:"))
-angle3 = float(input("Enter angle:"))
-
-if angle1 > 0 and angle2 > 0 and angle3 > 0 and (angle1+angle2+angle3 == 180):
-    print("The triangle is valid.")
-else:
-    print("The triangle is not valid.")

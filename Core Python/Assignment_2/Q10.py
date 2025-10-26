b260a9b67969e567adab3269b78b3a118c22308d
Yash-Patil25 <yp35385@gmail.com>
2025-10-26 17:55:31 +0530

    Delete Core Python/Assignment_2 directory

--- Core Python/Assignment_2/Q10.py
+++
@@ -1,9 +0,0 @@
-#Write a program to reverse three-digit number.
-num = int(input("enter three digit no:"))
-
-hundreds = num // 100
-tens = (num // 10) % 10
-units = num % 10
-
-reversed_num = (units * 100) + (tens * 10) + hundreds
-print("Reversed number:",reversed_num)
--- Core Python/Assignment_2/Q5.py
+++
@@ -1,7 +0,0 @@
-#WAP to calculate selling price of book based on cost price and discount.
-cost_price = float(input("Enter cost price of book:"))
-discount = float(input("Enter discount percentage:"))
-
-selling_price = cost_price - (cost_price * discount / 100)
-
-print("Selling price of book is",selling_price)
